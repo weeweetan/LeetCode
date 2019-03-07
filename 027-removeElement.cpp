3af9820b02fec8e8330c59f4d8259782d4f78685
tanwei <1113513062@qq.com>
2019-03-07 21:26:33 +0800

    027移除相同数据

--- 027-removeElement.cpp
+++ 027-removeElement.cpp
@@ -1,4 +1,31 @@
 //
 // Created by 11135 on 2019/2/25.
 //
+#include <vector>
+#include <iostream>
 
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    int temp[] = {0,1,2,2,3,0,4,2};
+    vector<int> vec(temp, temp+8);
+//    temp.push_back()
+    Solution solution;
+    int result = solution.removeElement(vec, 2);
+    cout << result << endl;
+}
