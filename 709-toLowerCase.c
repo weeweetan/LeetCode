36a683946293673cd3043412037cc94a101e2c0f
zhouji <286127084@qq.com>
2019-06-23 17:16:25 +0800

    709-toLowerCase函数实现

---
+++ 709-toLowerCase.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
+
+//
+// Created by 11135 on 2019/6/23.
+//
+char * toLowerCase(char * str) {
+    size_t str_len = strlen(str);
+//    char *o_str = malloc(str_len);
+    for (; *str != '\0'; str++)
+    {
+        if (*str >= 65 && *str <= 90)
+        {
+            *str = *str + 32;
+        }
+    }
+    return str - str_len;
+}
+
+
+int main(int argc, char **argv)
+{
+    char temp[] = {"Hello"};
+    char *o_str = toLowerCase(temp);
+    printf("%s\n", o_str);
+}
