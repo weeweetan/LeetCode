01eb2c4bcea1b9b76c1beaebea7014c6350f5030
tanwei <1113513062@qq.com>
2020-01-30 10:42:43 +0800

    237-deleteNode.c

--- 237-deleteNode.c
+++ 237-deleteNode.c
@@ -2,3 +2,23 @@
 // Created by 11135 on 2020/1/27.
 //
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+void deleteNode(struct ListNode* node)
+{
+    *node = *(node->next);
+}
