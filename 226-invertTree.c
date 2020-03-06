8d38b2a3979f886fefbf16f6d495a4a056b142b5
tanwei <1113513062@qq.com>
2020-03-06 23:19:34 +0800

    翻转二叉树

---
+++ 226-invertTree.c
@@ -0,0 +1,40 @@
+//
+// Created by 11135 on 2020/3/3.
+//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+#include <stdio.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    struct TreeNode *left = invertTree(root->left);
+    struct TreeNode *right = invertTree(root->right);
+
+    root->right = left;
+    root->left = right;
+
+}
+
+int main(int argc, char **argv)
+{
+    
+}
