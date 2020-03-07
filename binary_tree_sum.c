72801876f0cef9a124852bcc9f68989d405412ad
tanwei <1113513062@qq.com>
2020-03-07 20:35:25 +0800

    二叉树之和

--- binary_tree_sum.c
+++ binary_tree_sum.c
@@ -1,4 +1,119 @@
 //
 // Created by 11135 on 2020/3/6.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+typedef struct node
+{
+    struct node *left;
+    struct node *right;
+    int val;
+}Node;
+
+
+
+
+int binary_tree_sum(Node *root, int value, int *result, int index)
+{
+    int temp = 0;
+
+    if(root == NULL)
+        return 0;
+
+    temp += binary_tree_sum(root->left, value, result, index++);
+    temp += binary_tree_sum(root->right, value, result, index++);
+
+    temp += root->val;
+
+    if (temp == value)
+    {
+        result[index] = 1;
+    }
+
+    return temp;
+}
+
+bool match_subtree_sum(Node *root, int value)
+{
+    int result[100] = {0};
+
+    binary_tree_sum(root,  value, result, 0);
+
+    for (int i = 0; i < 100; ++i) {
+        if (result[i] == 1)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void pre_order_traversal(Node *root)
+{
+    if (root)
+    {
+        printf("%d ", root->val);
+        pre_order_traversal(root->left);
+        pre_order_traversal(root->right);
+    }
+}
+
+
+void mid_order_traversal(Node *root)
+{
+    if (root)
+    {
+        mid_order_traversal(root->left);
+        printf("%d ", root->val);
+        mid_order_traversal(root->right);
+    }
+}
+
+
+void after_order_traversal(Node *root)
+{
+    if (root)
+    {
+        after_order_traversal(root->left);
+        after_order_traversal(root->right);
+        printf("%d ", root->val);
+    }
+}
+
+int main(int argc, char **argv) {
+    Node *root = malloc(sizeof(Node));
+    Node *left = malloc(sizeof(Node));
+    Node *right = malloc(sizeof(Node));
+    Node *left1 = malloc(sizeof(Node));
+    Node *left2 = malloc(sizeof(Node));
+    Node *right1 = malloc(sizeof(Node));
+    Node *right2 = malloc(sizeof(Node));
+    Node *right3 = malloc(sizeof(Node));
+
+
+
+    root->val = 1;
+    left->val = 2;
+    right->val = 3;
+    left1->val = 4;
+    right1->val = 5;
+    left2->val = 6;
+    right2->val = 7;
+    right3->val = 8;
+
+    root->left = left;
+    root->right = right;
+    left->left = left1;
+    left->right = right1;
+    right->left = left2;
+    right->right = right2;
+    left2->right = right3;
+    printf("%d\n", match_subtree_sum(root, 24));
+//    pre_order_traversal(root);
+//    printf("\n");
+//    mid_order_traversal(root);
+//    printf("\n");
+//    after_order_traversal(root);
+}
