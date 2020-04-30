edca1de1f5e04088bf3bdcb06d943ac77e23de40
tanwei <1113513062@qq.com>
2020-04-30 22:13:08 +0800

    104-maxDepth

---
+++ 104-maxDepth.c
@@ -0,0 +1,60 @@
+//
+// Created by 11135 on 2020/4/30.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int maxDepth(struct TreeNode* root) {
+
+    int left, right;
+    if (root == NULL)
+        return 0;
+
+    left = maxDepth(root->left);
+
+    right = maxDepth(root->right);
+
+    return left > right ? left + 1 : right + 1;
+}
+
+typedef struct TreeNode Node;
+
+int main(int argc, char **argv)
+{
+    Node *root = malloc(sizeof(Node));
+    Node *left = malloc(sizeof(Node));
+    Node *right = malloc(sizeof(Node));
+    Node *left1 = malloc(sizeof(Node));
+    Node *left2 = malloc(sizeof(Node));
+    Node *right1 = malloc(sizeof(Node));
+    Node *right2 = malloc(sizeof(Node));
+    Node *right3 = malloc(sizeof(Node));
+
+
+
+    root->val = 1;
+    left->val = 2;
+    right->val = 3;
+    left1->val = 4;
+    right1->val = 5;
+    left2->val = 6;
+    right2->val = 7;
+    right3->val = 8;
+
+    root->left = left;
+    root->right = right;
+    left->left = left1;
+    left->right = right1;
+    right->left = left2;
+    right->right = right2;
+    left2->right = right3;
+
+    printf("%d\n", maxDepth(root));
+}
