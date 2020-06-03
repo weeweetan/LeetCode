74540b93dae2340f0dd1516ac1e951b2e07b0adf
tanwei <1113513062@qq.com>
2020-06-03 17:23:53 +0800

    112-hasPathSum

---
+++ 112-hasPathSum.c
@@ -0,0 +1,24 @@
+//
+// Created by 11135 on 2020/5/25.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+
+bool hasPathSum(struct TreeNode* root, int sum) {
+
+    if (root == NULL) return false;
+
+    sum -= root->val;
+
+    if (root->left == NULL && root->right == NULL && sum == 0) return true;
+
+    return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
+}
