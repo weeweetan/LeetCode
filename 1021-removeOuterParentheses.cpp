934756831c0a77f2ed9b42affdcd3d08896bf7e1
tanwei <1113513062@qq.com>
2020-09-25 19:48:28 +0800

    1021-removeOuterParentheses.cpp

---
+++ 1021-removeOuterParentheses.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 11135 on 2020/9/23.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        int left = 1, right = 0;
+        string res;
+
+        for (int i = 1; i < S.size(); ++i) {
+            if (S[i] == '(') left++;
+            else right++;
+
+            if (left != right) res.push_back(S[i]);
+            else {
+                i++;
+                left = 1;
+                right = 0;
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    string test = "(()())(())";
+    Solution s;
+    cout << s.removeOuterParentheses(test) << endl;
+}
