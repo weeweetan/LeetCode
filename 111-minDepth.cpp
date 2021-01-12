3254502267376e0aced6e85377869c82a2307957
tanwei <1113513062@qq.com>
2021-01-12 22:08:54 +0800

    111-minDepth.cpp

---
+++ 111-minDepth.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 11135 on 2021/1/10.
+//
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        int depth = 1;
+
+        while (!q.empty())
+        {
+            int sz = q.size();
+            for (int i = 0; i < sz; i++) {
+                TreeNode * cur = q.front();
+                q.pop();
+                if (cur->left == nullptr && cur->right == nullptr)
+                    return depth;
+                if (cur->left != nullptr)
+                    q.push(cur->left);
+                if (cur->right != nullptr)
+                    q.push(cur->right);
+
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
