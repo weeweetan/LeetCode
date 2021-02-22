2710aef5be3f5c2414ff0bb163d8a1c4e7701876
tanwei <1113513062@qq.com>
2021-02-22 21:58:15 +0800

    003-lengthOfLongestSubstring.cpp

---
+++ 003-lengthOfLongestSubstring.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 11135 on 2021/2/22.
+//
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> window;
+
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.size())
+        {
+            char c = s[right];
+            right++;
+            window[c]++;
+            while (window[c] > 1)
+            {
+                char d = s[left];
+                left++;
+                window[d]--;
+            }
+            res = max(res, right - left);
+        }
+
+        return res;
+    }
+};
