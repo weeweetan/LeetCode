56fe9fb3f19fad08ef384fe18ed620a92e10cb0b
tanwei <1113513062@qq.com>
2021-02-27 22:19:14 +0800

    046-permute.cpp

---
+++ 046-permute.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 11135 on 2021/2/27.
+//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& res, vector<int>& output, int first, int len)
+    {
+        if (first == len)
+        {
+            res.emplace_back(output);
+            return;
+        }
+
+        for (int i = first; i < len; i++) {
+            swap(output[i], output[first]);
+            backtrack(res, output, first + 1, len);
+            swap(output[i], output[first]);
+        }
+
+
+    }
+
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>res;
+        backtrack(res, nums, 0, nums.size());
+        return res;
+    }
+};
