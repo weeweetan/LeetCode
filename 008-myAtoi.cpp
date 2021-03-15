17411a3921d316753bf3e6b791f53e4eb2043a5d
tanwei <1113513062@qq.com>
2021-03-15 21:07:02 +0800

    008-myAtoi.cpp

---
+++ 008-myAtoi.cpp
@@ -0,0 +1,58 @@
+//
+// Created by 11135 on 2021/3/14.
+//
+#include <iostream>
+#include <string>
+#include <climits>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Automaton {
+    string state = "start";
+    unordered_map<string, vector<string>> table = {
+            {"start", {"start", "signed", "in_number", "end"}},
+            {"signed", {"end", "end", "in_number", "end"}},
+            {"in_number", {"end", "end", "in_number", "end"}},
+            {"end", {"end", "end", "end", "end"}}
+    };
+
+    int get_col(char c) {
+        if (isspace(c)) return 0;
+        if (c == '+' or c == '-') return 1;
+        if (isdigit(c)) return 2;
+        return 3;
+    }
+public:
+    int sign = 1;
+    long long ans = 0;
+
+    void get(char c) {
+        state = table[state][get_col(c)];
+        if (state == "in_number") {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? min(ans, (long long)INT_MAX) : min(ans, -(long long)INT_MIN);
+        }
+        else if (state == "signed")
+            sign = c == '+' ? 1 : -1;
+    }
+};
+
+class Solution {
+public:
+    int myAtoi(string str) {
+        Automaton automaton;
+        for (char c : str)
+            automaton.get(c);
+        return automaton.sign * automaton.ans;
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+    string string1 = "4193 with words";
+    Solution s;
+    cout << s.myAtoi(string1) << endl;
+}
