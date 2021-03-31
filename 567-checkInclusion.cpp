fe6702fc6fdd91471b7b30f47ff11b1a16f23619
tanwei <1113513062@qq.com>
2021-03-31 23:12:29 +0800

    567-checkInclusion.cpp

---
+++ 567-checkInclusion.cpp
@@ -0,0 +1,46 @@
+//
+// Created by 11135 on 2021/2/25.
+//
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> need, window;
+        for (char c : s1) need[c]++;
+
+        int left = 0, right = 0;
+        int valid = 0;
+
+        while (right < s2.size())
+        {
+            char c = s2[right];
+            right++;
+            if (need.count(c)) {
+                window[c]++;
+                if (window[c] == need[c])
+                    valid++;
+            }
+
+            while (right - left >= s1.size())
+            {
+                if (valid == need.size())
+                    return true;
+                char d = s2[left];
+                left++;
+                if (need.count(d))
+                {
+                    if (window[d] == need[d])
+                        valid--;
+                    window[d]--;
+                }
+            }
+        }
+        return false;
+    }
+};
